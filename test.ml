7543dc8c42783444c4f31e024c52d6976bd67440
RSAsterix <asterix_for_moderator@hotmail.com>
2016-02-25 14:44:08 +0100

    Test.ml gefixt

--- test.ml
+++ test.ml
@@ -1,40 +1,23 @@
 open Printf
 open Tokenizer
 open Parser
+open Char_func
 
 (* === code die file reading regelt. === *)
 (* Hierna kun je een regel uit het bestand krijgen door *)
 (* (Stream.next lines) aan te roepen *)
 
-let filename = "C:/Users/Martin/workspace/test/input.txt";;
-(*let filename = "./input.txt";;*)
+let filename = "C:/Users/tom_e/workspace/Project/input.txt";;
+(* let filename = "C:/Users/Martin/workspace/test/input.txt";; *)
 
-
-(* === string to char list === *)
-(* explode maakt een char list, implode een string *)
-(* nodig voor pattern matching op strings *) 
-
-
-	
-
-
-let tokenlist=[];
-let in_channel = open_in filename in
+let in_channel = open_in filename;;
+let tokenlist = ref [] in
 try
   while true do
     let line = input_line in_channel in
-		tokenlistline= scan_line (explode line)
+		let tokenlistline = scan_line (explode line) in
     print_endline (token_list_to_string (tokenlistline));
-		tokenlist = append tokenlist tokenlistline
-  done
+		tokenlist := List.append !tokenlist tokenlistline
+	done
 with End_of_file ->
-  close_in in_channel; 
-
-
-
-	
-
-(*in de def van exp staat dat een exp een char kan zijn. Ik snap niet waarvoor dat is*)
-(*let er op bij de compiler maken, dat onze infix operators gestructureerd zijn van links naar rechts ipv * boven + *)
-(*waarom staat er een id achter Fargs?*)	 
-
+  close_in in_channel;;
