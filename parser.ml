06f9a79543dc2bd990c0dd97f7e0ed7b1aff4298
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-11 10:13:16 +0100

    regelnummers toegevoegd aan parser

--- parser.ml
+++ parser.ml
@@ -125,27 +125,31 @@ funcall_parser list =
 (* 		| '[' type ']'        *)
 (* basictype = 'Int' | 'Bool' | 'Char' *)
 let rec type_parser = function
-	| (Basictoken a)::list -> Success (Basictype a),list
-	| (IDtok id)::list -> Success (Type_id (Id id)),list
-	| OPEN_PAR::list -> 
+	| (_,Basictoken a)::list -> Success (Basictype a),list
+	| (_,IDtok id)::list -> Success (Type_id (Id id)),list
+	| (l0,OPEN_PAR)::list -> 
 		(match (type_parser list) with
-		| Success type1,(COMMA::list) -> 
+		| Success type1, (l1,COMMA)::list -> 
 			(match (type_parser list) with
-			| Success type2,(CLOSE_PAR::list) -> Success (Type_tuple (type1,type2)),list
-			| Success _, list -> Error ("(type_parser) No closing parenthesis, but: " ^ token_list_to_string list), list
+			| Success type2, (_,CLOSE_PAR)::list -> Success (Type_tuple (type1,type2)),list
+			| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing parenthesis, but: " ^ token_to_string x), (l,x)::list
+			| Success _, [] -> Error ("(r." ^ string_of_int l1 ^ ") Unexpected EOF after comma."), []
 			| Error e, list -> Error e, list)
-		| Success _, list -> Error ("(type_parser) No closing parenthesis, but: " ^ token_list_to_string list), list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing parenthesis, but: " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after comma."), []
 		| Error e, list -> Error e, list)
-	| OPEN_BRACK::list -> 
+	| (l0,OPEN_BRACK)::list -> 
 		(match (type_parser list) with
-		| Success(type1),(CLOSE_BRACK::list) -> Success (Type_list type1),list
-		| Success type1 ,list -> Error ("(type_parser) No closing bracket, but: " ^ token_list_to_string list), list
+		| Success type1, (_,CLOSE_BRACK)::list -> Success (Type_list type1), list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing bracket, but: " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after opening bracket."), [] 
 		| Error e, list -> Error e, list)
-	| list -> Error ("(type_parser) Unexpected token: " ^ token_list_to_string list), list;;
+	| (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") Unexpected token: " ^ token_to_string x), (l,x)::list
+	| [] -> Error "Unexpected EOF while parsing type.", [];;
 
 (* rettype = 'Void' | type *)
 let rettype_parser = function
-	| VOID::list -> Success Type_void, list
+	| (_,VOID)::list -> Success Type_void, list
 	| list -> 
 		(match type_parser list with
 		| Success type1, list -> Success (Rettype type1), list
@@ -153,7 +157,7 @@ let rettype_parser = function
 
 (* varDeclRest = id '=' exp ';' *)
 let rec funtype_parser type_list = function
-  | ARROW::list	-> 
+  | (_,ARROW)::list	-> 
 		(match rettype_parser list with
   	| Success rettype, list -> Success (Funtype ((List.rev type_list), rettype)), list
   	| Error e, list -> Error e, list)
@@ -164,18 +168,20 @@ let rec funtype_parser type_list = function
 
 (* varDeclRest = id '=' exp ';' *)
 let vardecl_rest_parser typetoken = function
-	| IDtok id::EQ::list -> 
+	| (_,IDtok id)::(l0,EQ)::list -> 
 		(match exp_parser list with
-		| Success exp, SEMICOLON::list -> Success (Vardecl (typetoken, Id id, exp)), list
-		| Success _, list -> Error ("(vardecl_rest_parser) No semicolon, but: " ^ token_list_to_string list), list 
+		| Success exp, (_,SEMICOLON)::list -> Success (Vardecl (typetoken, Id id, exp)), list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No semicolon, but: " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after =."), []   
 		| Error e, list -> Error e, list)
-	| IDtok id::list -> Error ("(vardecl_rest_parser) No =, but " ^ token_list_to_string list), list
-	| list -> Error ("(vardecl_rest_parser) No id, but " ^ token_list_to_string list), list
+	| (_,IDtok id)::(l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No =, but " ^ token_to_string x), (l,x)::list
+	| (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No id, but " ^ token_to_string x), (l,x)::list
+	| [] -> Error "Unexpected EOF when parsing the rest of the variable declaration.", []
 
 (* VarDecl = 'var' VarDeclRest *)
 (* 		| type VarDeclRest      *)
 let vardecl_parser = function
-  | VAR::list -> vardecl_rest_parser None list
+  | (_,VAR)::list -> vardecl_rest_parser None list
   | list -> 
 		(match type_parser list with
 		| Success type1, list -> vardecl_rest_parser (Some type1) list
@@ -197,97 +203,111 @@ let rec vardecl_list_parser vardecl_list = function
 (* 		| id '(' funcall                                        *)
 (* 		| id field '=' exp                                      *)
 let rec stmt_list_parser stmt_list = function
-	| CLOSE_ACO::list -> Success (List.rev stmt_list), list 
+	| (_,CLOSE_ACO)::list -> Success (List.rev stmt_list), list 
 	| list ->
 		(match stmt_parser list with
 		| Success stmt, list -> stmt_list_parser (stmt::stmt_list) list
 		| Error e, list -> Error e, list)
 and
 stmt_parser = function
-  | IF::OPEN_PAR::list ->
+  | (_,IF)::(l0,OPEN_PAR)::list ->
   	(match exp_parser list with
-		| Success exp, CLOSE_PAR::OPEN_ACO::list ->
+		| Success exp, (_,CLOSE_PAR)::(_,OPEN_ACO)::list ->
 			(match stmt_list_parser [] list with
-			| Success stmt_list1, ELSE::OPEN_ACO::list ->
+			| Success stmt_list1, (_,ELSE)::(_,OPEN_ACO)::list ->
 				(match stmt_list_parser [] list with
 				| Success stmt_list2, list -> Success (Stmt_if_else (exp, stmt_list1, stmt_list2)), list
 				| Error e, list -> Error e, list)
+			| Success _, (_,ELSE)::(l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No opening acolade, but " ^ token_to_string x), (l,x)::list
 			| Success stmt_list1, list -> Success (Stmt_if (exp, stmt_list1)), list
 			| Error e, list -> Error e, list)
-		| Success _, list -> Error ("(stmt_parser) Unexpected token after expression: " ^ token_list_to_string list), list
+		| Success _, (_,CLOSE_PAR)::(l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No opening acolade, but " ^ token_to_string x), (l,x)::list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing parenthesis, but " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after opening parenthesis."), []
 		| Error e, list -> Error e, list)
-	| WHILE::OPEN_PAR::list ->
+	| (_,WHILE)::(l0,OPEN_PAR)::list ->
 		(match exp_parser list with
-		| Success exp, CLOSE_PAR::OPEN_ACO::list ->
+		| Success exp, (_,CLOSE_PAR)::(_,OPEN_ACO)::list ->
 			(match stmt_list_parser [] list with
 			| Success stmt_list, list -> Success (Stmt_while (exp, stmt_list)), list
 			| Error e, list -> Error e, list)
-		| Success _, list -> Error ("(stmt_parser) Unexpected token after expression: " ^ token_list_to_string list), list
+		| Success _, (_,CLOSE_PAR)::(l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No opening acolade, but " ^ token_to_string x), (l,x)::list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing parenthesis, but " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after opening parenthesis."), [] 
 		| Error e, list -> Error e, list)
-	| RETURN::SEMICOLON::list -> Success (Stmt_return(None)), list
-	| RETURN::list ->
+	| (_,RETURN)::(_,SEMICOLON)::list -> Success (Stmt_return(None)), list
+	| (l0,RETURN)::list ->
 		(match exp_parser list with
-		| Success exp, SEMICOLON::list -> Success (Stmt_return(Some(exp))), list
-		| Success _, list -> Error ("(stmt_parser) No semicolon, but: " ^ token_list_to_string list), list
+		| Success exp, (_,SEMICOLON)::list -> Success (Stmt_return(Some(exp))), list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No semicolon, but: " ^ token_to_string x), list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after parsing 'return'."), [] 
 		| Error e, list -> Error e, list)
-	| (IDtok id)::OPEN_PAR::list -> 
+	| (_,IDtok id)::(_,OPEN_PAR)::list -> 
   	(match funcall_parser list with
   	| Success exp_list, list -> Success (Stmt_function_call (Id id, exp_list)), list
 		| Error e, list -> Error e, list)
-	| (IDtok id)::list ->
+	| (l0,IDtok id)::list ->
 		(match (field_parser [] list) with
-		| Success fieldlist, EQ::list -> 
+		| Success fieldlist, (l1,EQ)::list -> 
     	(match exp_parser list with
-    	| Success exp, SEMICOLON::list -> Success (Stmt_define (Id id, fieldlist, exp)), list
-    	| Success _, list -> Error ("(stmt_parser) No semicolon, but: " ^ token_list_to_string list), list
+    	| Success exp, (_,SEMICOLON)::list -> Success (Stmt_define (Id id, fieldlist, exp)), list
+    	| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No semicolon, but: " ^ token_to_string x), (l,x)::list
+			| Success _, [] -> Error ("(r." ^ string_of_int l1 ^ ") Unexpected EOF after parsing '='."), []   
 			| Error e, list -> Error e, list)
-		| Success _, list -> Error ("(stmt_parser) No '=', but: " ^ token_list_to_string list), list
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No '=', but: " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after parsing " ^ id ^ "."), [] 
 		| Error e, list -> Error e, list)
-	| list -> Error ("(stmt_parser) Unexpected token: " ^ token_list_to_string list), list;;
+	| (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") Unexpected token: " ^ token_to_string x), list
+	| [] -> Error ("Unexpected EOF while parsing statement."), []
 
 (* fargs =   ')' | fargs2          *)
 (* fargs2 = id ')' | id ',' fargs2 *)
 (*opgesplitst in twee functies, zodat fargs ook '()' mag zijn*)
 let fargs_parser list =
 	let rec fargs2_parser id_list = function
-		| (IDtok id)::CLOSE_PAR::list	-> Success (Fargs (List.rev ((Id id)::id_list))),list
-  	| (IDtok id)::COMMA::list -> fargs2_parser ((Id id)::id_list) list 
-		| list -> Error ("(fargs_parser) No closing parenthesis or comma, but: " ^ token_list_to_string list), list in
-  match list with
-	| CLOSE_PAR::list	-> Success (Fargs []),list
+		| (_,IDtok id)::(_,CLOSE_PAR)::list	-> Success (Fargs (List.rev ((Id id)::id_list))),list
+  	| (_,IDtok id)::(_,COMMA)::list -> fargs2_parser ((Id id)::id_list) list 
+		| (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No closing parenthesis or comma, but: " ^ token_to_string x), (l,x)::list
+		| [] -> Error ("Unexpected EOF when parsing function arguments."), []
+  in match list with
+	| (_,CLOSE_PAR)::list -> Success (Fargs []), list
 	| list -> fargs2_parser [] list
 
 (* funDecl = fargs '(' vardeclList stmt stmtList     *)
 (* 		| fargs '::' funtype '(' vardeclList stmtList *)
 let fundecl_parser id list = match fargs_parser list with
-  | Error e, faillist -> Error e, faillist
-  | Success fargs, OPEN_ACO::list ->
+  | Error e, list -> Error e, list
+  | Success fargs, (l0,OPEN_ACO)::list ->
   	(match vardecl_list_parser [] list with
-		| Error e, faillist -> Error e, faillist
+		| Error e, list -> Error e, list
   	| Success vardecl_list, list ->
   		(match stmt_list_parser [] list with
-			| Error e, VAR::faillist -> Error ("(fundecl_parser) Previous vardecl parse failed. " ^ e), faillist
-  		| Error e, faillist -> Error e, faillist
-  		| Success [], lastlist -> Error ("(fundecl_parser) No statement, but: " ^ token_list_to_string lastlist), lastlist
-  		| Success stmt_list, lastlist -> Success (Fundecl (id,fargs,None,vardecl_list,stmt_list)),lastlist))
-  | Success fargs,DDPOINT::list ->
+			| Error e, (l,VAR)::list -> Error ("(r." ^ string_of_int l ^ ") Previous variable decleration parse failed, with error:\n" ^ e), list
+  		| Error e, list -> Error e, list
+  		| Success [], (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") Not the beginning of a statement, but: " ^ token_to_string x), (l,x)::list
+			| Success [], [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after parsing opening acolade."), [] 
+  		| Success stmt_list, list -> Success (Fundecl (id, fargs, None, vardecl_list, stmt_list)), list))
+  | Success fargs, (l0,DDPOINT)::list ->
     (match funtype_parser [] list with
-    | Error e, faillist -> Error e, faillist
-    | Success funtype, OPEN_ACO::list ->
+    | Error e, list -> Error e, list
+    | Success funtype, (l1,OPEN_ACO)::list ->
       (match vardecl_list_parser [] list with
-			| Error e, faillist -> Error e, faillist
+			| Error e, list -> Error e, list
       | Success vardecl_list, list ->
         (match stmt_list_parser [] list with
-				| Error e, VAR::faillist -> Error ("(fundecl_parser) Previous vardecl parse failed. " ^ e), faillist
-        | Error e, faillist -> Error e, faillist
-        | Success [], lastlist -> Error ("(fundecl_parser) No statement, but: " ^ token_list_to_string lastlist), lastlist
-        | Success stmt_list, lastlist -> Success (Fundecl (id,fargs,Some funtype,vardecl_list,stmt_list)),lastlist))
-		| Success funtype, list -> Error ("(fundecl_parser) No opening parenthesis, but: " ^ token_list_to_string list), list)
-	| Success fargs, list -> Error ("(fundecl_parser) No opening parenthesis or ::, but: " ^ token_list_to_string list), list;;
+				| Error e, (l,VAR)::list -> Error ("(r." ^ string_of_int l ^ ") Previous variable declaration parse failed, with error:\n" ^ e), list
+        | Error e, list -> Error e, list
+        | Success [], (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No statement, but: " ^ token_to_string x), (l,x)::list
+				| Success [], [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after parsing opening acolade."), [] 
+        | Success stmt_list, list -> Success (Fundecl (id, fargs, Some funtype, vardecl_list, stmt_list)), list))
+		| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No opening acolade, but: " ^ token_to_string x), (l,x)::list
+		| Success _, [] -> Error ("(r." ^ string_of_int l0 ^ ") Unexpected EOF after parsing '::'."), [])
+	| Success _, (l,x)::list -> Error ("(r." ^ string_of_int l ^ ") No opening parenthesis or ::, but: " ^ token_to_string x), (l,x)::list
+	| Success _, [] -> Error ("Unexpected EOF while parsing function declaration."), [];;
 
 (* Decl = id '('  FunDecl | VarDecl *)
 let decl_parser = function
-	| IDtok id::OPEN_PAR::list ->
+	| (_,IDtok id)::(_,OPEN_PAR)::list ->
 		(match fundecl_parser (Id id) list with
 		| Success fundecl, list -> Success (Decl_fun fundecl), list
 		| Error e, faillist -> Error e, faillist)
