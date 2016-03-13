f8fa5abe34d480790d078d8d0b503e9328dd49c3
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-13 13:09:40 +0100

    Hulpfuncties toegevoegd

--- typechecker_lib.ml
+++ typechecker_lib.ml
@@ -7,13 +7,16 @@ type types =
 	| Lis of types
 	| Int | Bool | Char;;
 
-let rec remove_dups lst = match lst with 
-| [] -> []
-| h::t -> h::(remove_dups (List.filter (fun x -> x<>h) t));;
+let rec remove_dups lst = 
+	match lst with
+	| [] -> []
+	| h::t -> h::(remove_dups (List.filter (fun x -> x<>h) t));;
 
-let rec print_list = function 
-[] -> ()
-| e::l -> print_int e ; print_string " " ; print_list l;;
+let rec print_list = function
+	| [] -> ()
+	| e::l -> print_int e ; print_string " " ; print_list l;;
+
+let isIn el lst = List.exists (fun x -> x = el) lst;;  
 
 let rec print_type out = function
 	| Var i -> fprintf out "%i" i
