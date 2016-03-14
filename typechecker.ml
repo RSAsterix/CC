ecaaa1df0e99535d22eb637dba04862c26e249ed
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-14 12:24:25 +0100

    "u" is af, "m" lijkt te werken voor expressies

--- typechecker.ml
+++ typechecker.ml
@@ -45,63 +45,71 @@ let tv t =
 	remove_dups (tv_help [] t);;
 
 let u t1 t2 =
-	let rec u t = function
+	let rec u_help list t = function
   	| Var a ->
   		(match t with
-  		| Var a1 when (a = a1) -> List.rev list
-  		| x when (not isIn a (tv x)) -> List.rev ((a,x)::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Var a) ^ ".")
+  		| Var a1 when (a = a1) -> Success (List.rev list)
+  		| x when (not (isIn a (tv x))) -> Success (List.rev ((a,x)::list))
+			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Var a) ^ "."))
   	| Imp (t1,t2) ->
 			(match t with
 			| Imp (s1, s2) ->
-				(match u s2 t2 with
+				(match u_help [] s2 t2 with
 				| Success x ->
-					(match u (substitute x s1) (substitute x t1) with
+					(match u_help [] (substitute x s1) (substitute x t1) with
 					| Success left -> Success (o left x)
-					| Error e -> ("Unable to unify arguments, due to:\n" ^ e))
+					| Error e -> Error ("Unable to unify arguments, due to:\n" ^ e))
 				| Error e -> Error ("Unable to unify result, due to:\n" ^ e))
-			| Var a when (not isIn a (tv (Imp (t1,t2)))) -> List.rev ((a,t)::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Imp (t1,t2)) ^ ".")
+			| Var a when (not (isIn a (tv (Imp (t1,t2))))) -> Success (List.rev ((a,Imp (t1,t2))::list))
+			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Imp (t1,t2)) ^ "."))
   	| Tup (t1,t2) ->
 			(match t with
 			| Tup (s1, s2) ->
-				(match u s2 t2 with
+				(match u_help [] s2 t2 with
 				| Success x ->
-					(match u (substitute x s1) (substitute x t1) with
+					(match u_help [] (substitute x s1) (substitute x t1) with
 					| Success left -> Success (o left x)
-					| Error e -> ("Unable to unify right side of tuples, due to:\n" ^ e))
+					| Error e -> Error ("Unable to unify right side of tuples, due to:\n" ^ e))
 				| Error e -> Error ("Unable to unify left side of tuples, due to:\n" ^ e))
+			| Var a when (not (isIn a (tv (Tup (t1,t2))))) -> Success (List.rev ((a,Tup (t1,t2))::list))
+			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Tup (t1,t2)) ^ "."))
   	| Lis t1 ->
-			
-  	| -> 
+			(match t with
+			| Lis s1 -> u_help [] s1 t1
+			| Var a when (not (isIn a (tv (Lis t1)))) -> Success (List.rev ((a,(Lis t1))::list))
+			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Lis t1) ^ "."))
+  	| t1 ->
+			(match t with
+			| Var a when (not (isIn a (tv t1))) -> Success (List.rev ((a,t1)::list))
+			| t2 when (t1 = t2) -> Success (List.rev list)
+			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type t1 ^ ".")) in
+	u_help [] t1 t2;;
 		
-
-
-let u tuple = 
-	let rec u_help list = function
-	(* | (Var a1, Var a2) when (a1 = a2) -> List.rev list *)
-	| (Var a, t) when (not (isIn a (tv t))) -> List.rev ((a,t)::list)
-	(* | (t, Var a) when (not (isIn a (tv t))) -> List.rev ((a,t)::list) *)
-	| (Imp (s1, s2), Imp (t1, t2)) -> 
-		(let x = u_help [] (s2, t2) in
-		(let u1 = u_help [] (substitute x s1, substitute x t1) in
-		(let result = o u1 x in
-		List.append result list)))
-	| (Tup (s1, s2), Tup (t1, t2)) ->
-		(let x = u_help [] (s2, t2) in
-		(let u1 = u_help [] (substitute x s1, substitute x t1) in
-		(let result = o u1 x in
-		List.append result list)))
-	| (Lis s, Lis t) ->
-		(let result = u_help [] (s, t) in
-		List.append result list)
-	| (Int, Int) -> List.rev list
-	| (Bool, Bool) -> List.rev list
-	| (Char, Char) -> List.rev list
-	| tuple -> [("error",Var "error")] (* heel cheatsy *) in 
-	match u_help [] tuple with
-	| list when (not (List.exists (fun x -> x = ("error",Var "error")) list)) -> Success list
-	| _ -> Error "Type error";;
+(* let u tuple =                                                                               *)
+(* 	let rec u_help list = function                                                            *)
+(* 	(* | (Var a1, Var a2) when (a1 = a2) -> List.rev list *)                                  *)
+(* 	| (Var a, t) when (not (isIn a (tv t))) -> List.rev ((a,t)::list)                         *)
+(* 	(* | (t, Var a) when (not (isIn a (tv t))) -> List.rev ((a,t)::list) *)                   *)
+(* 	| (Imp (s1, s2), Imp (t1, t2)) ->                                                         *)
+(* 		(let x = u_help [] (s2, t2) in                                                          *)
+(* 		(let u1 = u_help [] (substitute x s1, substitute x t1) in                               *)
+(* 		(let result = o u1 x in                                                                 *)
+(* 		List.append result list)))                                                              *)
+(* 	| (Tup (s1, s2), Tup (t1, t2)) ->                                                         *)
+(* 		(let x = u_help [] (s2, t2) in                                                          *)
+(* 		(let u1 = u_help [] (substitute x s1, substitute x t1) in                               *)
+(* 		(let result = o u1 x in                                                                 *)
+(* 		List.append result list)))                                                              *)
+(* 	| (Lis s, Lis t) ->                                                                       *)
+(* 		(let result = u_help [] (s, t) in                                                       *)
+(* 		List.append result list)                                                                *)
+(* 	| (Int, Int) -> List.rev list                                                             *)
+(* 	| (Bool, Bool) -> List.rev list                                                           *)
+(* 	| (Char, Char) -> List.rev list                                                           *)
+(* 	| tuple -> [("error",Var "error")] (* heel cheatsy *) in                                  *)
+(* 	match u_help [] tuple with                                                                *)
+(* 	| list when (not (List.exists (fun x -> x = ("error",Var "error")) list)) -> Success list *)
+(* 	| _ -> Error "Type error";;                                                               *)
 
 
 (* type exp =                             *)
@@ -116,22 +124,22 @@ let u tuple =
 let rec m env exp = function
 	| var -> m_help env var exp
 and m_help env var = function
-	| Exp_int _ -> u (var, Int)
-	| Exp_bool _ -> u (var, Bool)
-	| Exp_char _ -> u (var, Char)
+	| Exp_int _ -> u var Int
+	| Exp_bool _ -> u var Bool
+	| Exp_char _ -> u var Char
 	| Exp_infix (e1, (Op2 op), e2) when (is_op_plus op || is_op_times op) ->
 		(match m env e1 Int with
 		| Success x1 ->
 			(match m (substitute x1 env) e2 Int with
 			| Success r1 ->
 				(let x = o r1 x1 in
-				(match u (substitute x var, Int) with
+				(match u (substitute x var) Int with
 				| Success r2 -> Success (o r2 x)
 				| Error e -> Error ("Complete expression not an int because of:\n" ^ e)))
 			| Error e -> Error ("Second part of expression not an int because of:\n" ^ e))
 		| Error e -> Error ("First part of expression not an int because of:\n" ^ e))
 	| _ -> Error "Unsupported expression";;
 
-match (m Int (Exp_infix (Exp_bool true, Op2 "+", (Exp_int (Inttoken 2)))) (Var "b")) with
+match (m Int (Exp_infix ((Exp_bool true), Op2 "+", (Exp_int (Inttoken 2)))) (Var "b")) with
 | Success x -> print_subs stdout x;
 | Error e -> print_string e;;
--- typechecker_lib.ml
+++ typechecker_lib.ml
@@ -12,20 +12,11 @@ let rec remove_dups lst =
 	| [] -> []
 	| h::t -> h::(remove_dups (List.filter (fun x -> x<>h) t));;
 
-let rec print_type out = function
-	| Var s -> fprintf out "%s" s
-	| Imp (t1,t2) -> fprintf out "%a -> %a" print_type t1 print_type t2
-	| Tup (t1,t2) -> fprintf out "(%a,%a)" print_type t1 print_type t2
-	| Lis t -> fprintf out "[%a]" print_type t
-	| Int -> fprintf out "int"
-	| Bool -> fprintf out "bool"
-	| Char -> fprintf out "char";;
-
 let rec string_of_type = function
 	| Var s -> sprintf "%s" s
-	| Imp (t1,t2) -> sprintf "%a -> %a" print_type t1 print_type t2
-	| Tup (t1,t2) -> sprintf "(%a,%a)" print_type t1 print_type t2
-	| Lis t -> sprintf "[%a]" print_type t
+	| Imp (t1,t2) -> sprintf "%s -> %s" (string_of_type t1) (string_of_type t2)
+	| Tup (t1,t2) -> sprintf "(%s,%s)" (string_of_type t1) (string_of_type t2)
+	| Lis t -> sprintf "[%s]" (string_of_type t)
 	| Int -> sprintf "int"
 	| Bool -> sprintf "bool"
 	| Char -> sprintf "char";;
@@ -34,7 +25,7 @@ let rec string_of_type = function
 let print_subs out subs =
 	let rec subs_print_help out = function
 	| [] -> ()
-	| [(x,nx)] -> fprintf out "%s |-> %a" x print_type nx
+	| [(x,nx)] -> fprintf out "%s |-> %s" x (string_of_type nx)
 	| el::xs -> fprintf out "%a, %a" subs_print_help [el] subs_print_help xs in
 	fprintf out "[%a]" subs_print_help subs;;
 
