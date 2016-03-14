da46be414cff6c03c2a82f84ada34193ee09db27
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-14 15:39:22 +0100

    Opgeschoond

--- typechecker.ml
+++ typechecker.ml
@@ -1,6 +1,7 @@
 open Typechecker_lib
 open Types
 open Char_func
+open Printf
 
 (* nieuwe variabele genereren *)
 let c = ref 0;;
@@ -8,7 +9,7 @@ let v = ref "";;
 let fresh = function
 	| _ -> 
 		c := !c + 1;
-		v := "f" ^ string_of_int !c;;
+		v := sprintf "%if" !c;;
 
 (* herschrijfregel uit subs gebruiken   *)
 (* subs = [x1 |-> nx1; x2 |-> nx2; ...] *) 
@@ -50,7 +51,7 @@ let u t1 t2 =
   		(match t with
   		| Var a1 when (a = a1) -> Success (List.rev list)
   		| x when (not (isIn a (tv x))) -> Success (List.rev ((a,x)::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Var a) ^ "."))
+			| _ -> Error (sprintf "Found type '%s' where type '%s' was expected." (string_of_type (Var a)) (string_of_type t)))
   	| Imp (t1,t2) ->
 			(match t with
 			| Imp (s1, s2) ->
@@ -61,7 +62,7 @@ let u t1 t2 =
 					| Error e -> Error ("Unable to unify arguments, due to:\n" ^ e))
 				| Error e -> Error ("Unable to unify result, due to:\n" ^ e))
 			| Var a when (not (isIn a (tv (Imp (t1,t2))))) -> Success (List.rev ((a,Imp (t1,t2))::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Imp (t1,t2)) ^ "."))
+			| _ -> Error (sprintf "Found type '%s' where type '%s' was expected." (string_of_type (Imp (t1,t2))) (string_of_type t)))
   	| Tup (t1,t2) ->
 			(match t with
 			| Tup (s1, s2) ->
@@ -72,65 +73,31 @@ let u t1 t2 =
 					| Error e -> Error ("Unable to unify right side of tuples, due to:\n" ^ e))
 				| Error e -> Error ("Unable to unify left side of tuples, due to:\n" ^ e))
 			| Var a when (not (isIn a (tv (Tup (t1,t2))))) -> Success (List.rev ((a,Tup (t1,t2))::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Tup (t1,t2)) ^ "."))
+			| _ -> Error (sprintf "Found type '%s' where type '%s' was expected." (string_of_type (Tup (t1,t2))) (string_of_type t)))
   	| Lis t1 ->
 			(match t with
 			| Lis s1 -> u_help [] s1 t1
 			| Var a when (not (isIn a (tv (Lis t1)))) -> Success (List.rev ((a,(Lis t1))::list))
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type (Lis t1) ^ "."))
+			| _ -> Error (sprintf "Found type '%s' where type '%s' was expected." (string_of_type (Lis t1)) (string_of_type t)))
   	| t1 ->
 			(match t with
 			| Var a when (not (isIn a (tv t1))) -> Success (List.rev ((a,t1)::list))
 			| t2 when (t1 = t2) -> Success (List.rev list)
-			| _ -> Error ("Trying to unify " ^ string_of_type t ^ " with " ^ string_of_type t1 ^ ".")) in
+			| _ -> Error (sprintf "Found type '%s' where type '%s' was expected." (string_of_type t1) (string_of_type t))) in
 	u_help [] t1 t2;;
-		
-(* let u tuple =                                                                               *)
-(* 	let rec u_help list = function                                                            *)
-(* 	(* | (Var a1, Var a2) when (a1 = a2) -> List.rev list *)                                  *)
-(* 	| (Var a, t) when (not (isIn a (tv t))) -> List.rev ((a,t)::list)                         *)
-(* 	(* | (t, Var a) when (not (isIn a (tv t))) -> List.rev ((a,t)::list) *)                   *)
-(* 	| (Imp (s1, s2), Imp (t1, t2)) ->                                                         *)
-(* 		(let x = u_help [] (s2, t2) in                                                          *)
-(* 		(let u1 = u_help [] (substitute x s1, substitute x t1) in                               *)
-(* 		(let result = o u1 x in                                                                 *)
-(* 		List.append result list)))                                                              *)
-(* 	| (Tup (s1, s2), Tup (t1, t2)) ->                                                         *)
-(* 		(let x = u_help [] (s2, t2) in                                                          *)
-(* 		(let u1 = u_help [] (substitute x s1, substitute x t1) in                               *)
-(* 		(let result = o u1 x in                                                                 *)
-(* 		List.append result list)))                                                              *)
-(* 	| (Lis s, Lis t) ->                                                                       *)
-(* 		(let result = u_help [] (s, t) in                                                       *)
-(* 		List.append result list)                                                                *)
-(* 	| (Int, Int) -> List.rev list                                                             *)
-(* 	| (Bool, Bool) -> List.rev list                                                           *)
-(* 	| (Char, Char) -> List.rev list                                                           *)
-(* 	| tuple -> [("error",Var "error")] (* heel cheatsy *) in                                  *)
-(* 	match u_help [] tuple with                                                                *)
-(* 	| list when (not (List.exists (fun x -> x = ("error",Var "error")) list)) -> Success list *)
-(* 	| _ -> Error "Type error";;                                                               *)
 
 
-(* type exp =                             *)
-(* 	| Exp_field of id * field            *)
-(* 	| Exp_infix of exp * op2 * exp       *)
-(* 	| Exp_prefix of op1 * exp            *)
-(* 	| Exp_function_call of id * exp list *)
-(* 	| Exp_emptylist                      *)
-(* 	| Exp_tuple of exp * exp             *)
-
-
+(* Env: (x,a,t) *)
 let rec m env exp = function
 	| var -> m_help env var exp
 and m_help env var = function
-	| Exp_int _ -> u var Int
-	| Exp_bool _ -> u var Bool
-	| Exp_char _ -> u var Char
+	| Exp_int _ -> u Int var
+	| Exp_bool _ -> u Bool var
+	| Exp_char _ -> u Char var
 	| Exp_infix (e1, (Op2 op), e2) when (is_op_plus op || is_op_times op) ->
 		(match m env e1 Int with
 		| Success x1 ->
-			(match m (substitute x1 env) e2 Int with
+			(match m (substitute x1 env) e2 Int with (* Wat staat er in die environment? *)
 			| Success r1 ->
 				(let x = o r1 x1 in
 				(match u (substitute x var) Int with
