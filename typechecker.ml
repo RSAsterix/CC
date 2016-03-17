8fdc6f09caa0fc5f1b947b306e2d46ab7bae922c
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-17 13:18:59 +0100

    Listop en logop added

--- typechecker.ml
+++ typechecker.ml
@@ -3,7 +3,10 @@ open Types
 open Char_func
 open Printf
 
-(* nieuwe variabele genereren *)
+(* nieuwe variabele genereren:*)
+(* roep eerst fresh() aan*)
+(* gebruik vervolgens "Var !v" voor een verse variabele*)
+(* dit gaat goed, omdat een normale Var nooit met een cijfer kan beginnen*)
 let c = ref 0;;
 let v = ref "";;
 let fresh = function
@@ -92,15 +95,16 @@ let u t1 t2 =
 
 (* Env: (x,a,t) ? *)
 let rec m env exp = function
-	| var -> m_help env var exp
+	| var -> m_exp env var exp
 and m_exp env var = function
 	| Exp_int _ -> u Int var
 	| Exp_bool _ -> u Bool var
 	| Exp_char _ -> u Char var
+	| Exp_emptylist -> fresh(); u var (Lis (Var !v)) 
 	| Exp_infix (e1, op, e2) ->
 		(match op with
 		| Listop ->
-			fresh;
+			fresh();
 			(match m env e1 (Var !v) with
 			| Success x1 ->
 				(match m (substitute x1 env) e2 (Lis (substitute x1 (Var !v))) with
@@ -111,36 +115,20 @@ and m_exp env var = function
 					| Error e -> Error ("List ill-typed because of:\n" ^ e)))
 				| Error e -> Error ("Tail ill-typed because of:\n" ^ e))
 			|	Error e -> Error ("Head ill-typed because of:\n" ^ e))
-		| Logop ->
+		| Logop _ ->
 			(match m env e1 Bool with
 			| Success x1 ->
 				(match m (substitute x1 env) e2 Bool with
 				| Success res1 ->
 					(let x = o res1 x1 in
-					
-				| Error e -> Error ("Second part of expression ill-typed because of:\n" ^ e))
-			| Error e -> Error ("First part of expression ill-typed because of:\n" ^ e))
-					 
-			
-
-
-		
-		
-		
-		
-		
-		(match m env e1 Int with
-		| Success x1 ->
-			(match m (substitute x1 env) e2 Int with (* Wat staat er in die environment? *)
-			| Success r1 ->
-				(let x = o r1 x1 in
-				(match u (substitute x var) Int with
-				| Success r2 -> Success (o r2 x)
-				| Error e -> Error ("Complete expression not an int because of:\n" ^ e)))
-			| Error e -> Error ("Second part of expression not an int because of:\n" ^ e))
-		| Error e -> Error ("First part of expression not an int because of:\n" ^ e))
+					(match u (substitute x var) Bool with
+					| Success res2 -> Success (o res2 x)
+					| Error e -> Error ("Expression not a boolean because of:\n" ^ e))) 
+				| Error e -> Error ("Second part of expression not a boolean because of:\n" ^ e))
+			| Error e -> Error ("First part of expression not a boolean because of:\n" ^ e))
+		| _ -> Error "Operator not supported yet.")
 	| _ -> Error "Unsupported expression";;
 
-match (m Int (Exp_infix ((Exp_bool true), Op2 "+", (Exp_int (Inttoken 2)))) (Var "b")) with
+match (m Int (Exp_infix (Exp_bool true, Listop, Exp_emptylist)) (Var "b")) with
 | Success x -> print_subs stdout x;
 | Error e -> print_string e;;
