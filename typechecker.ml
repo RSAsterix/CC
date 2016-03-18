d26123068b559a807d9b06d20d23fa2c49f32d20
RSAsterix <asterix_for_moderator@hotmail.com>
2016-03-18 11:26:39 +0100

    infix and prefix klaar

--- typechecker.ml
+++ typechecker.ml
@@ -12,33 +12,27 @@ and m_exp env var = function
 	| Exp_char _ -> u Char var
 	| Exp_emptylist -> fresh(); u var (Lis (Var !v))
 	| Exp_infix (e1, op, e2) ->
-		(match op with
-		| Listop ->
-			fresh();
-			(match m env e1 (Var !v) with
+		(let (opL, opR, opRES) = op2_to_subs op in
+		(match m env e1 opL with
 			| Success x1 ->
-				(match m (substitute x1 env) e2 (Lis (substitute x1 (Var !v))) with
+				(match m (substitute x1 env) e2 (substitute x1 opR) with
 				| Success res1 ->
 					(let x = o res1 x1 in
-					(match u (substitute x var) (substitute x (Lis (Var !v))) with
+					(match u (substitute x var) (substitute x opRES) with
 					| Success res2 -> Success (o res2 x)
-					| Error e -> Error ("List ill-typed because of:\n" ^ e)))
-				| Error e -> Error ("Tail ill-typed because of:\n" ^ e))
-			|	Error e -> Error ("Head ill-typed because of:\n" ^ e))
-		| Logop _ ->
-			(match m env e1 Bool with
-			| Success x1 ->
-				(match m (substitute x1 env) e2 Bool with
-				| Success res1 ->
-					(let x = o res1 x1 in
-					(match u (substitute x var) Bool with
-					| Success res2 -> Success (o res2 x)
-					| Error e -> Error ("Expression not a boolean because of:\n" ^ e))) 
-				| Error e -> Error ("Second part of expression not a boolean because of:\n" ^ e))
-			| Error e -> Error ("First part of expression not a boolean because of:\n" ^ e))
-		| _ -> Error "Operator not supported yet.")
+					| Error e -> Error ("Complete expression ill-typed because of:\n" ^ e)))
+				| Error e -> Error ("Right part ill-typed because of:\n" ^ e))
+			|	Error e -> Error ("Left part ill-typed because of:\n" ^ e)))
+	| Exp_prefix (op, e1) ->
+		(let opRES = op1_to_subs op in
+		(match m env e1 opRES with
+		| Success x ->
+			(match u (substitute x var) opRES with
+			| Success res1 -> Success (o res1 x)
+			| Error e -> Error ("Negative ill-typed because of:\n" ^ e))
+		| Error e -> Error ("Value ill-typed because of:\n" ^ e)))
 	| _ -> Error "Unsupported expression";;
 
-match (m Int (Exp_infix (Exp_bool true, Listop, (Exp_infix (Exp_bool false, Listop, Exp_emptylist)))) (Var "b")) with
+match (m Int (Exp_prefix (Not, Exp_bool true)) (Var "b")) with
 | Success x -> print_subs stdout x;
 | Error e -> print_string e;;
